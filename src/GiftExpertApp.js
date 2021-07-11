242346011e55958e6ba03b46f3f2f97aa0717eaf
José Monar <jl_mj_17@hotmail.com>
2021-07-11 01:23:31 -0500

    Ejercicio, uso hook de estado para agregar nuevo elemento a un arreglo y mostrarlo en un <ul>.

--- src/GiftExpertApp.js
+++ src/GiftExpertApp.js
@@ -1,16 +1,31 @@
-// import PropTypes from "prop-types";
+import { useState } from "react";
 
 const GiftExpertApp = () => {
+    const [categories, setCategories] = useState([
+        "One Punch",
+        "Naruto",
+        "Dragon Ball",
+    ]);
+
+    const handleAdd = () => {
+        // setCategories([...categories, "Ranma"]);
+        setCategories((cats) => [...cats, "Ranma 1/2"]);
+    };
+
     return (
         <>
             <h2>GiftExpertApp</h2>
             <hr />
+
+            <button onClick={handleAdd}>Agregar</button>
+
+            <ul>
+                {categories.map((category) => {
+                    return <li key={category}> {category}</li>;
+                })}
+            </ul>
         </>
     );
 };
-
-// GiftExpertApp.propTypes = {
-//     value,
-// };
 
 export default GiftExpertApp;
